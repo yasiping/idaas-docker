6dc5dc05f80306366fde6a3b980a2ce613af0466
王春晓 <wangcxscut@163.com>
2021-03-22 16:09:31 +0800

    add idaas-base

--- build_prepare.sh
+++ build_prepare.sh
@@ -10,9 +10,18 @@ ftp -n -i 192.168.238.112 <<EOF
 
 user ywkf ywkf
 binary
+cd /ftp/mc-idaas/dbtoolbuild/package
+lcd ${PWD}/idaas_package
+mget *
+
 cd /ftp/mc-idaas/version/
 lcd ${PWD}/
 mget mc-center.tar
+
+
+cd /ftp/mc-idaas/idaas-package/py_package/
+lcd ${PWD}/py_package
+mget *
 
 bye
 
@@ -20,9 +29,14 @@ EOF
 
 echo "Down packge from FTP successfully."
 
-rm -rf APP/cmdb-db/tmp/
+rm -rf APP/idaas-db/tmp/
+rm -rf APP/idaas-base/tmp/py_package
+rm -rf APP/idaas-base/tmp/package
 
 tar -zvxf mc-center.tar
+
+cp -r idaas_package APP/idaas-base/tmp/package
+cp -r py_package APP/idaas-base/tmp/
 
 
 
