c7faf92af4bc174024bef3312ca3658cfbc60c26
王春晓 <wangcxscut@163.com>
2021-03-24 08:59:34 +0800

    delete default application

--- APP/idaas/tmp/start.sh
+++ APP/idaas/tmp/start.sh
@@ -6,7 +6,7 @@ echo "########################################################"
 
 cd /capaa
 bash ./install.sh
-mv -f /application.yml /data/mc-center/conf/application.yml
+#mv -f /application.yml /data/mc-center/conf/application.yml
 
 cd /capaa/mc-center/bin/
 bash ./startup_mc_center.sh
