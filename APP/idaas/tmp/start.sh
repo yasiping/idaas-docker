20f047c5470b2d6ceeffa7ae6f465279f51ab781
wangchunxiao <wangcxscut@163.com>
2021-05-12 16:02:48 +0800

    feat add config file

--- APP/idaas/tmp/start.sh
+++ APP/idaas/tmp/start.sh
@@ -2,11 +2,15 @@
 
 echo "########################################################"
 
+
 #/wait-for-it.sh osm-db:3306 -t 0
 
 cd /capaa
 bash ./install.sh
-#mv -f /application.yml /data/mc-center/conf/application.yml
+
+mv -f /application.yml /data/mc-center/conf/application.yml
+mv -f /mcApp.json /data/mc-center/conf/mcApp.json
+
 
 cd /capaa/mc-center/bin/
 bash ./startup_mc_center.sh
